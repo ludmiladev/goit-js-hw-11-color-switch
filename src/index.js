3efe5cd3094d254e63d1f25bfc020d996d3c4c1a
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-11-02 12:52:46 +0200

    видалила ссилку з редмі

--- src/index.js
+++ src/index.js
@@ -1,45 +1,36 @@
-import './styles.css';
+import css from "./css/style.css";
 
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-]
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
-let backgroundInterval = null;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 const start = document.querySelector('[data-action="start"]');
 const stop = document.querySelector('[data-action="stop"]');
 const body = document.querySelector('body');
 
-start.addEventListener('click', onColorSwitchStart);
-stop.addEventListener('click', OnColorSwichStop);
+let colorTime;
 
-stop.disabled = true;
 
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-function bacgroundSwich() {
-    return body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
-
-};
-
-function onColorSwitchStart() {
-    backgroundInterval = setInterval(bacgroundSwich, 1000);
-
-    stop.disabled = false;
-    start.disabled = true;
-};
-
-function OnColorSwichStop() {
-    clearInterval(backgroundInterval);
-
-    stop.disabled = true;
-    start.disabled = false;
-
+const color = () => {
+  
+  colorTime = setInterval(()=> {
+    body.style.backgroundColor = `${colors[randomIntegerFromInterval(0, 5)]}`;
+    start.removeEventListener("click", color);
+  }, 1000);
 }
+
+start.addEventListener('click', color);
+stop.addEventListener('click', () => {
+  clearInterval(colorTime);
+  start.addEventListener('click', color);
+});
+
